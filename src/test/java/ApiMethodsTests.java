d600fe2cd8b96792b31240581d17cce6a8e2d7f1
Pritam Patil <pritam.patil@optile.net>
2020-01-03 17:32:43 +0100

    added more api tests

--- src/test/java/ApiMethodsTests.java
+++ src/test/java/ApiMethodsTests.java
@@ -4,14 +4,16 @@ import org.testng.annotations.Test;
 
 import io.restassured.RestAssured;
 import io.restassured.http.Method;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 
-
 public class ApiMethodsTests {
 
+    int statusCode;
+
     @Test
-    void getUserDetails() {
+    void getUserDetailsTest() {
         RestAssured.baseURI = "https://reqres.in/api/users/";
 
         RequestSpecification httpRequest = RestAssured.given();
@@ -22,15 +24,13 @@ public class ApiMethodsTests {
 
         String responseBody = response.getBody().asString();
 
-        int statusCode = response.getStatusCode();
+        statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 200);
-
-        System.out.println("Response - " + responseBody);
 
     }
 
     @Test
-    void createUser() {
+    void createUserTest() {
         RestAssured.baseURI = "https://reqres.in/api/users/";
 
         RequestSpecification httpRequest = RestAssured.given();
@@ -45,8 +45,67 @@ public class ApiMethodsTests {
 
         Response response = httpRequest.request(Method.POST);
 
-        int statusCode = response.getStatusCode();
+        statusCode = response.getStatusCode();
         Assert.assertEquals(statusCode, 201);
 
     }
+
+    @Test
+    void checkAuthorizationTest() {
+
+        RestAssured.baseURI = "https://postman-echo.com/basic-auth";
+
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.request(Method.GET, "/");
+
+        statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 401);
+
+        httpRequest.auth().preemptive().basic("postman", "password");
+
+        Response responseWithAuth = httpRequest.request(Method.GET, "/");
+        statusCode = responseWithAuth.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+
+        JsonPath respeone = responseWithAuth.jsonPath();
+
+        Boolean authentication = respeone.get("authenticated");
+        Assert.assertTrue(authentication);
+    }
+
+    @Test
+    void updateUserDetailsTest() {
+
+        RestAssured.baseURI = "https://reqres.in/api/users/";
+
+        RequestSpecification httpRequest = RestAssured.given();
+
+        JSONObject updateData = new JSONObject();
+        updateData.put("name", "Aarna");
+
+        httpRequest.header("Content-Type", "application/json");
+
+        httpRequest.body(updateData.toJSONString());
+        Response response = httpRequest.request(Method.PUT, "4");
+        statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 200);
+
+        JsonPath newData = response.jsonPath();
+        String name = newData.get("name");
+        Assert.assertEquals(name, "Aarna");
+
+    }
+
+    @Test
+    void deleteUserTest() {
+        RestAssured.baseURI = "https://reqres.in/api/users/";
+
+        RequestSpecification httpRequest = RestAssured.given();
+
+        Response response = httpRequest.request(Method.DELETE, "4");
+        statusCode = response.getStatusCode();
+        Assert.assertEquals(statusCode, 204);
+    }
+
 }
